21011bd92c9454f7ae9fae223ef89f9b485d48eb
atrem13 <mertayoga17.my@gmail.com>
2021-09-02 11:40:53 +0800

    create and index transaction done

--- src/views/transaction/Create.vue
+++ src/views/transaction/Create.vue
@@ -1,10 +1,91 @@
 <template>
-    <div>
-        <h3>Ini Create transaction</h3>
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <router-link 
+                    :to="{ name:'transaction.index' }"
+                    class="btn btn-secondary btn-sm rounded shadow-sm my-3"
+                >
+                    Back
+                </router-link>
+                <div class="card rounded shadow-sm">
+                    <div class="card-header">
+                        Create Transaction
+                    </div>
+                    <div class="card-body">
+                        <form @submit.prevent="store()">
+                            <div class="mb-3">
+                                <label for="" class="form-label">Title</label>
+                                <input type="text" class="form-control" v-model="transaction.title">
+                                <div v-if="validation.title" class="text-danger">
+                                    {{ validation.title[0] }}
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="" class="form-label">Amount</label>
+                                <input type="text" class="form-control" v-model="transaction.amount">
+                                <div v-if="validation.amount" class="text-danger">
+                                    {{ validation.amount[0] }}
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="" class="form-label">Type</label>
+                                <select class="form-control" v-model="transaction.type">
+                                    <option value="expense">Expense</option>
+                                    <option value="revenue">Revenue</option>
+                                </select>
+                                <div v-if="validation.type" class="text-danger">
+                                    {{ validation.type[0] }}
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <button class="btn btn-sm btn-primary" type="submit">Submit</button>
+                            </div>
+                        </form>                        
+                    </div>
+                </div>
+            </div>    
+        </div>
     </div>
 </template>
+
 <script>
+import axios from 'axios'
+import {reactive, ref} from 'vue'
+import {useRouter} from 'vue-router'
+
 export default {
-    
+    setup() {
+        let transaction = reactive({
+            title: '',
+            amount: '',
+            type: '',
+        });
+
+        let validation = ref([]);
+
+        let router = useRouter();
+        
+        function store() {
+            axios.post(
+                'http://127.0.0.1:8000/api/transactions',
+                transaction
+            )
+            .then((result) => {
+                router.push({
+                    name: 'transaction.index',
+                });
+            }).catch((err) => {
+                validation.value = err.response.data;
+            });
+        };        
+
+        return {
+            transaction,
+            store,
+            validation,
+            router
+        }
+    }
 }
 </script>
--- src/views/transaction/Index.vue
+++ src/views/transaction/Index.vue
@@ -1,10 +1,86 @@
 <template>
-    <div>
-        <h3>Ini index transaction</h3>
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <router-link 
+                    :to="{ name:'transaction.create' }"
+                    class="btn btn-primary btn-sm rounded shadow-sm my-3"
+                >
+                    Add New Transaction
+                </router-link>
+                <div class="card rounded shadow-sm">
+                    <div class="card-header">
+                        Transaction List
+                    </div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Amount</th>
+                                    <th>Type</th>
+                                    <th>Action</th>
+                                </tr>  
+                            </thead>
+                            <tbody>
+                                <tr v-for="(transaction, index) in transactions" :key="index">
+                                    <td>{{ transaction.title }}</td>
+                                    <td>{{ transaction.amount }}</td>
+                                    <td>{{ transaction.type }}</td>
+                                    <td>
+                                        <div class="btn-group">
+                                            <router-link 
+                                                :to="{name: 'transaction.edit', params: {id: transaction.id}}"
+                                                class="btn btn-sm btn-outline-primary mr-2" 
+                                            >
+                                                Edit
+                                            </router-link>
+                                        </div>  
+                                        <div class="btn-group">
+                                            <router-link 
+                                                :to="{name: 'transaction.edit', params: {id: transaction.id}}"
+                                                class="btn btn-sm btn-outline-primary mr-2" 
+                                            >
+                                                Delete
+                                            </router-link>
+                                        </div>   
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>    
+        </div>
     </div>
 </template>
+
 <script>
+import axios from 'axios'
+import {onMounted, ref} from 'vue'
+
 export default {
-    
+    setup() {
+        let transactions = ref([]);
+        
+        onMounted(() => {
+            axios.get('http://127.0.0.1:8000/api/transactions')
+            .then((result) => {
+                transactions.value = result.data.data;
+                console.log(transactions.value);
+            }).catch((err) => {
+                console.log(err.response);
+            });
+        });
+
+        function DeleteData() {
+
+        };        
+
+        return {
+            transactions,
+            onMounted
+        }
+    }
 }
 </script>
